eaf025ab1f583fc408a21b13b284bcbc46c753ce
Manh Pham <manhpham90vn@icloud.com>
2024-11-04 18:18:32 +0700

    refactor

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.s3_bucket
 }
 
+provider "aws" {
+  alias  = "global"
+  region = "us-east-1"
+}
+
 resource "aws_s3_bucket_ownership_controls" "bucket_ownership_controls" {
   bucket = aws_s3_bucket.s3_bucket.id
 
@@ -13,18 +18,20 @@ resource "aws_s3_bucket_ownership_controls" "bucket_ownership_controls" {
 resource "aws_s3_bucket_public_access_block" "bucket_public_access_block" {
   bucket = aws_s3_bucket.s3_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
   bucket = aws_s3_bucket.s3_bucket.id
   versioning_configuration {
-    status = "Disabled"
+    status = "Enabled"
   }
 }
+
+data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket_policy" "s3_bucket_policy" {
   bucket = aws_s3_bucket.s3_bucket.id
@@ -33,11 +40,18 @@ resource "aws_s3_bucket_policy" "s3_bucket_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "arn:aws:s3:::${aws_s3_bucket.s3_bucket.id}/*"
+        Sid    = "AllowCloudFrontAccess"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.s3_bucket.id}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" : "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.cloudfront_distribution.id}"
+          }
+        }
       }
     ]
   })
@@ -55,33 +69,90 @@ resource "aws_s3_bucket_website_configuration" "bucket_website_configuration" {
   }
 }
 
-resource "aws_s3_bucket_cors_configuration" "bucket_cors_configuratio" {
+resource "aws_s3_bucket_cors_configuration" "bucket_cors_configuration" {
   bucket = aws_s3_bucket.s3_bucket.id
 
   cors_rule {
-    allowed_methods = ["GET"]
-    allowed_origins = ["*"]
+    allowed_methods = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    allowed_origins = ["https://${var.domain_name}", "https://www.${var.domain_name}"]
+    allowed_headers = ["*"]
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "cloudfront_origin_access_control" {
+  name                              = "s3-origin-access-control"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_wafv2_web_acl" "wafv2_web_acl" {
+  provider = aws.global
+  name     = "waf-acl"
+  scope    = "CLOUDFRONT"
+  default_action {
+    allow {}
+  }
+  rule {
+    name     = "AWS-AWSManagedRulesCommonRuleSet"
+    priority = 1
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    override_action {
+      none {}
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesSQLiRuleSet"
+    priority = 2
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesSQLiRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    override_action {
+      none {}
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesSQLiRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  token_domains = [var.domain_name, "www.${var.domain_name}"]
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "waf"
+    sampled_requests_enabled   = true
   }
 }
 
 resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   origin {
-    domain_name = aws_s3_bucket.s3_bucket.bucket_regional_domain_name
-    origin_id   = "S3-${aws_s3_bucket.s3_bucket.id}"
-
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
-    }
+    domain_name              = aws_s3_bucket.s3_bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.cloudfront_origin_access_control.id
+    origin_id                = "S3-${aws_s3_bucket.s3_bucket.id}"
   }
 
   enabled             = true
   is_ipv6_enabled     = true
   default_root_object = "index.html"
-
-  aliases = [var.domain_name, "www.${var.domain_name}"]
+  web_acl_id          = aws_wafv2_web_acl.wafv2_web_acl.arn
+  aliases             = [var.domain_name, "www.${var.domain_name}"]
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
@@ -92,7 +163,7 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
       query_string = false
 
       cookies {
-        forward = "none"
+        forward = "all"
       }
     }
 
